ccf029f999707fe3f5c1a694ffc9bbb734b1acb5
AlexMorr1son <alex.bukin@hotmail.com>
2021-06-23 20:03:19 +0100

    small updates

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const slides = document.getElementsByClassName("slide");
 const slidesNum = slides.length;
 const rightButton = document.querySelector(".right-arrow");
+const leftButton = document.querySelector(".left-arrow");
 let currentSlide = "0";
 slides[0].className = "slide show-slide";
 
-const moveSlide = () => {
+const nextSlide = () => {
     slides[currentSlide].className = "slide";
     currentSlide = (currentSlide + 1) % slidesNum;
     slides[currentSlide].className = "slide show-slide";
+    clearInterval(timer);
+    timer = setInterval(nextSlide, 3500);
+}
+
+const prevSlide = () => {
+    slides[currentSlide].className = "slide";
+    currentSlide = (currentSlide === 0) ? currentSlide === slidesNum : (currentSlide - 1) % slidesNum; 
+    slides[currentSlide].className = "slide show-slide";
+    console.log(currentSlide);
+    clearInterval(timer);
+    timer = setInterval(nextSlide, 3500);
 }
 
 
-rightButton.addEventListener("click", moveSlide);
+rightButton.addEventListener("click", nextSlide);
+leftButton.addEventListener("click", prevSlide);
 
-// setInterval(moveSlide, 5000);
+let timer = setInterval(nextSlide, 3500);
