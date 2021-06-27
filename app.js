309f1a33011ec3a5a0dde6cf86ee607caa9e0595
AlexMorr1son <alex.bukin@hotmail.com>
2021-06-27 20:38:25 +0100

    updated animation

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const slides = document.getElementsByClassName("slide");
+const slider = document.querySelector("#slider");
 const slidesNum = slides.length;
 const rightButton = document.querySelector(".right-arrow");
 const leftButton = document.querySelector(".left-arrow");
 let currentSlide = "0";
-slides[0].className = "slide show-slide";
+
+
 
 const nextSlide = () => {
-    slides[currentSlide].className = "slide";
     currentSlide = (currentSlide + 1) % slidesNum;
-    slides[currentSlide].className = "slide show-slide";
-    clearInterval(timer);
-    timer = setInterval(nextSlide, 3500);
+    slider.style.transform = `translateX(${currentSlide * -600}px)`;
+    console.log(currentSlide, currentSlide * -600)
 }
 
 const prevSlide = () => {
-    slides[currentSlide].className = "slide";
-    currentSlide = (currentSlide === 0) ? currentSlide === slidesNum : (currentSlide - 1) % slidesNum; 
-    slides[currentSlide].className = "slide show-slide";
-    console.log(currentSlide);
-    clearInterval(timer);
-    timer = setInterval(nextSlide, 3500);
+    currentSlide = (currentSlide - 1) % slidesNum;
+    if (currentSlide === -1) {
+        currentSlide = 2
+    }
+    slider.style.transform = `translateX(${currentSlide * -600}px)`;
+    console.log(currentSlide, currentSlide * 600);
 }
 
 
 rightButton.addEventListener("click", nextSlide);
 leftButton.addEventListener("click", prevSlide);
 
-let timer = setInterval(nextSlide, 3500);
+// let timer = setInterval(nextSlide, 3500);
